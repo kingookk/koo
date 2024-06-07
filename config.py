a27608715f32d318fd7287686eb82a76a27bb611
kingookk <138496905+kingookk@users.noreply.github.com>
2024-06-07 05:05:09 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("10871792"))
+API_HASH = getenv("6f3f84d0b392900e09b0aed186470890")
+
+BOT_TOKEN = getenv("5378293477:AAFSuiJceFBukUgNZgvu9NPD9dEM6Zkhx9o")
+
